eb326a8aee5c09e0ba1295c341bf0c0b81579335
malhotra-repo <94556197+malhotra-repo@users.noreply.github.com>
2026-03-06 17:40:03 +0530

    first commit

---
+++ src/components/home/HomeHeroText.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Video from './Video'
+
+const HomeHeroText = () => {
+    return (
+    <div className='font-[font2]  pt-5 text-center'>
+
+        <div className= 'text-[9.5vw] uppercase justify-center flex items-center leading-[8vw]' > L'etincelle</div>
+
+        <div  className= 'text-[9.5vw] uppercase justify-center flex items-center leading-[8vw]'>qui 
+            
+            <div className='h-[7vw] w-[16vw] rounded-full -mt-3 overflow-hidden'><Video /></div>genere</div>
+
+        <div className= 'text-[9.5vw] uppercase justify-center flexitems-center leading-[8vw]' > la creativite</div >
+      
+    </div >
+  )
+}
+
+export default HomeHeroText
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Video from '../components/home/Video'
+import HomeBottomText from '../components/home/HomeBottomText'
+import HomeHeroText from '../components/home/HomeHeroText'
+
+const Home = () => {
+  return (
+    
+   <div>
+     <div className='h-screen w-screen  fixed'>
+      <Video />
+    </div>
+    <div className=' h-screen w-screen relative'>
+      <HomeHeroText />
+      <HomeBottomText/>
+
+    </div>
+   </div>
+  )
+}
+
+export default Home
