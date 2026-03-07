da9c3200a40c55ea2cdf8a742f29e32fbee863d0
malhotra-repo <spiderwebmr76@gmail.com>
2026-03-07 12:41:20 +0530

    auto push 07-03-2026 12:41

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>
+    <div className='font-[font2] flex items-center justify-center gap-2 bg-red-500'>
+      <Link className='text-[6vw] lg:mt-6 ' to='/agence'>Agence</Link>
+      <Link className='text-[6vw] lg:mt-6 ' to='/projects'>Projects</Link>
       
     </div>
   )
