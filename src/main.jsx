59bdbb2eae6f7d8086da09dada3a86f4f4103bbe
malhotra-repo <spiderwebmr76@gmail.com>
2026-03-09 14:26:42 +0530

    auto push 09-03-2026 14:26

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import Stairs from "./components/common/Stairs.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-<React.StrictMode>
-
-<BrowserRouter>
-<h1>jai ho</h1>
-<App/>
-</BrowserRouter>
-  </React.StrictMode>
-
-)
-  
-
-
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Stairs>
+        <App />
+      </Stairs>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
