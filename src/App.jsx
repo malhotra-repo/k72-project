b73e9beefdd1322e4140d67181828630e457fa0e
malhotra-repo <spiderwebmr76@gmail.com>
2026-03-10 13:40:24 +0530

    auto push 10-03-2026 13:40

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import Navbar from './components/Navigation/Navbar'
 
 
 const App = () => {
   return (
     <div className='text-white'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/agence' element={<Agence/>}/>
